6b3cca420f51eed2e8b15a00790a073a19bb4b17
David Peatman <davidpeatman@pm.me>
2021-04-01 14:33:25 -0700

    Udpating method visibility

--- Source/ObjectExtensions.cs
+++ Source/ObjectExtensions.cs
@@ -1,29 +1,27 @@
 #nullable enable
 
-using System;
-
 namespace ScopeFunctions
 {
-    static class ObjectExtensions 
+    public static class ObjectExtensions 
     {
-        public static R Let<T, R>(this T self, Func<T, R> block) 
+        public static R Let<T, R>(this T self, System.Func<T, R> block) 
         {
             return block(self);
         }
 
-        public static T Also<T>(this T self, Action block)
+        public static T Also<T>(this T self, System.Action block)
         {
             block();
             return self;
         }
 
-        public static T Also<T>(this T self, Action<T> block)
+        public static T Also<T>(this T self, System.Action<T> block)
         {
             block(self);
             return self;
         }
 
-        public static T  ElseThrow<T, E>(this T self, E exception) where E : Exception {
+        public static T  ElseThrow<T, E>(this T self, E exception) where E : System.Exception {
             return self ?? throw exception;
         }
     }
